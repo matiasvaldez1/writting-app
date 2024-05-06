52bac83c7f2071c89e2e1a7e7848dc11b99b60aa
Mati <matiasvaldez8184@gmail.com>
2024-05-06 02:48:22 -0300

    test api route build error

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic' // defaults to auto
+
 export const GET = (req: Request) => {
   console.log(req.body, req.formData, req.url);
   return new Response("hello world");
