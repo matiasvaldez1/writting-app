74f7b467122f22826bb30715e81beb339187f374
Mati <matiasvaldez8184@gmail.com>
2024-05-06 02:33:44 -0300

    fix build errors

--- src/app/_components/anim-writting-text.tsx
+++ src/app/_components/anim-writting-text.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { motion, useMotionValue, useTransform, animate } from "framer-motion";
 import { useEffect, useRef } from "react";
 
--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,11 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-type ResponseData = {
-  message: string;
-};
-
-export function GET(req: NextApiRequest, res: NextApiResponse<ResponseData>) {
-    console.log(req)
-  NextResponse.json({ message: "Hello from Next.js!" });
+export function GET(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.body, req.statusMessage, req.url)
+  return NextResponse.json({message: 'Hello!'})
 }
