0fb638dfff39974e3420187d1135039161a9b7a4
Mati <matiasvaldez8184@gmail.com>
2024-05-06 02:39:43 -0300

    build error

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,7 +1,4 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
-
-export function GET(req: NextApiRequest, res: NextApiResponse) {
-    console.log(req.body, req.statusMessage, req.url)
-  return NextResponse.json({message: 'Hello!'})
-}
+export const GET = (req: Request) => {
+  console.log(req.body, req.formData, req.url);
+  return new Response("hello world");
+};
