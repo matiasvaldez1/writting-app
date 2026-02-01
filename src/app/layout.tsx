de17816ee443832e9e9984fcd8a5185a4fbb6f39
unknown <matiasvaldez8184@gmail.com>
2026-02-01 18:39:54 -0300

    fix deprecated apple-mobile-web-app-capable meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default async function RootLayout({
           <link rel="icon" href="/favicon.ico" sizes="any" />
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content="#000000" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="mobile-web-app-capable" content="yes" />
           <meta
             name="apple-mobile-web-app-status-bar-style"
             content="default"
